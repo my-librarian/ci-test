bda3bad9cac98d401b3eab7610298563f0001ecd
Gopikrishna Sathyamurthy <sandalblack19@gmail.com>
2016-05-15 00:46:07 -0500

    Move if else to script file

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
-# find dist -type f -exec curl -u $USER:$PASS ftp://ftp.byethost7.com/my-librarian.is-best.net/htdocs/ -T {} \;
-ls
+## find dist -type f -exec curl -u $USER:$PASS ftp://ftp.byethost7.com/my-librarian.is-best.net/htdocs/ -T {} \;
+#ls
+
+if ([ "$TRAVIS_BRANCH" == "master" ] || [ ! -z "$TRAVIS_TAG" ]) &&
+    [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+  ls
+else
+  echo "This will not deploy!"
+fi
