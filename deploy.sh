bbcd243f0539bc262de7ddda013ec3f37fc87a0a
Gopikrishna Sathyamurthy <sandalblack19@gmail.com>
2016-05-15 00:23:36 -0500

    testing script

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,2 @@
-find dist -type f -exec curl -u $USER:$PASS ftp://ftp.byethost7.com/my-librarian.is-best.net/htdocs/ -T {} \;
+# find dist -type f -exec curl -u $USER:$PASS ftp://ftp.byethost7.com/my-librarian.is-best.net/htdocs/ -T {} \;
+ls dist
