22c5344173b8a696492c69d287f5f0a50e6f7b6c
Gopikrishna Sathyamurthy <sandalblack19@gmail.com>
2016-05-15 01:00:52 -0500

    Fix deploy conditions

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-if [ "$TRAVIS_BRANCH" = "master" ] && [ ! -z "$TRAVIS_TAG" ]; then
+if [ ! -z "$TRAVIS_TAG" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
   find dist -type f -exec curl -u $USER:$PASS $HOST -T {} \;
 else
   echo "This will not deploy!"
